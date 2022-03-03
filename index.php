52593a4816ff789c8749cf44741444637b523493
scomb116 <100876075+scomb116@users.noreply.github.com>
2022-03-03 14:32:16 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/loggin.css">
+    <title>Login</title>
+</head>
+<body>
+<div class="card">
+
+<form action="" class="card_Form_login" method="POST">
+<p>Login</p>
+    <label for="user"> Usuario</label> <br>
+    <input type="text" name="user" id="" placeholder="ej: leopoldo" autocomplete="off" autofocus> <br>
+    <label for="pass"> Contraseña</label> <br>
+    <input type="password"  name="Contraseña" id="" placeholder="ej: admin" autocomplete="off"><br>
+
+    <input type="submit" name="Ingresar" class="btn_submit">
+
+</form>
+
+
+</div>
+
+    
+</body>
+</html>
